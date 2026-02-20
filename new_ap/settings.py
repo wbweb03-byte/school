3fbd7be41d4b90c377a83d1e45664fbb4586a6a3
schoolmanage <wb.web03@gmail.com>
2026-02-20 17:10:08 +0530

    database update

--- new_ap/settings.py
+++ new_ap/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,13 +80,23 @@ WSGI_APPLICATION = 'new_ap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbname',
+        'USER': 'schoolmange_user',
+        'PASSWORD': 'bC1HVdYefX3F2zCuAxxU6m6vjUpQP62A',
+        'HOST': 'dpg-d6c4dpcr85hc73dsp2b0-a',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
