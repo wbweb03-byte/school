205f10c832245f0b9e60c9ec22d2c40da79be240
schoolmanage <wb.web03@gmail.com>
2026-02-27 10:33:23 +0530

    restart

--- new_ap/settings.py
+++ new_ap/settings.py
@@ -142,16 +142,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
 LOGIN_URL = 'login'      # name of your login url
 LOGIN_REDIRECT_URL = 'dashboard'
